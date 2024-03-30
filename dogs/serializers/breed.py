80e6da83f0773f3f44eea30deeb2b94b8559fba0
Mary Melnikova <marymelnikova1988@gmail.com>
2024-03-30 13:05:06 +0300

    Решение задач по теме «Сериализаторы и фильтры» количество собак в данной породе

--- dogs/serializers/breed.py
+++ dogs/serializers/breed.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from rest_framework.fields import SerializerMethodField
+from rest_framework.fields import SerializerMethodField, IntegerField
+from rest_framework.relations import SlugRelatedField
 
 from dogs.models import Breed, Dog
+# from dogs.serializers.dog import DogListSerializer
+
+class DogListSerializer(serializers.ModelSerializer):
+    breed = SlugRelatedField(slug_field='name', queryset=Breed.objects.all())
+    class Meta:
+        model = Dog
+        fields = ('name', 'breed')
 
 
 class BreedSerializer(serializers.ModelSerializer):
@@ -10,15 +18,19 @@ class BreedSerializer(serializers.ModelSerializer):
         fields = ('name',)
 
 class BreedListSerializer(serializers.ModelSerializer):
+    # чтобы добавилось количество собак в данной породе
+    dog_count = IntegerField()
     class Meta:
         model = Breed
-        fields = ('name',)
+        fields = ('name','dog_count',)
 
 class BreedDetailSerializer(serializers.ModelSerializer):
     dog_this_breed = SerializerMethodField()
 
     def get_dog_this_breed(self, breed):
-        return [dog.name for dog in Dog.objects.filter(breed=breed)]
+        # return [dog.name for dog in Dog.objects.filter(breed=breed)]
+        # делаем возвращение словарем через сериализатор
+        return DogListSerializer(Dog.objects.filter(breed=breed), many=True).data
     class Meta:
         model = Breed
         fields = "__all__"
--- dogs/views/breed.py
+++ dogs/views/breed.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from rest_framework.viewsets import ModelViewSet
 
 from dogs.serializers.breed import *
@@ -5,6 +6,8 @@ from dogs.serializers.breed import *
 
 class BreedViewSet(ModelViewSet):
     queryset = Breed.objects.all()
+    # переопределяем каврисет, чтобы добавилось количество собак в данной породе / добавили функцию на лист вместо этого/
+    # queryset = Breed.objects.annotate(dog_count=Count("dog"))
     # serializer_class = BreedDetailSerializer
     default_serializer = BreedSerializer
     serializers = {
@@ -14,3 +17,8 @@ class BreedViewSet(ModelViewSet):
 
     def get_serializer_class(self, *args, **kwargs):
         return self.serializers.get(self.action, self.default_serializer)
+
+    def list(self, request, *args, **kwargs):
+        self.queryset = self.queryset.annotate(dog_count=Count("dog"))
+        return super().list(request, *args, **kwargs)
+
