f05916d49e0584b29ae772b2156b4dcd44711fd9
Mary Melnikova <marymelnikova1988@gmail.com>
2024-04-01 22:31:48 +0300

    25.10.1 валидация

--- dogs/serializers/dog.py
+++ dogs/serializers/dog.py
@@ -4,9 +4,11 @@ from rest_framework.relations import SlugRelatedField
 
 from dogs.models import Dog, Breed
 from dogs.serializers.breed import BreedDetailSerializer
+from dogs.validators import validator_scam_words
 
 
 class DogSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(validators=[validator_scam_words])
     class Meta:
         model = Dog
         fields = "__all__"
